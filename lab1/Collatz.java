7d0c3214c4af3340eb26937b24f1037e88e76ff1
Ibrahem Elhdad <hemaelhadad26@gmail.com>
2024-02-17 15:36:14 +0200

    proj1 test

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -4,11 +4,14 @@
 public class Collatz {
 
     public static int nextNumber(int n) {
-        if (n %2 == 0) {
-            return n*2/4*5;
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
+            return 3 * n + 1;
         } else {
-            return 2 * n + 1;
+            return n * 2;
         }
+    }
     }
 
     public static void main(String[] args) {
