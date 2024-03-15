fb81bf8062edab101f240d22f7ada340d7892741
Ibrahem Elhdad <hemaelhadad26@gmail.com>
2024-03-15 14:53:18 +0200

    proj 2

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -1,8 +1,31 @@
 package gitlet;
+import jdk.jshell.execution.Util;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import gitlet.Utils.*;
+import java.io.FileInputStream;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Formatter;
+import java.util.List;
 
 // TODO: any imports you need here
 
 import java.util.Date; // TODO: You'll likely use this in this class
+
+import static gitlet.Utils.*;
+
 
 /** Represents a gitlet commit object.
  *  TODO: It's a good idea to give a description here of what else this Class
@@ -10,17 +33,47 @@ import java.util.Date; // TODO: You'll likely use this in this class
  *
  *  @author TODO
  */
-public class Commit {
+public class Commit implements Serializable{
     /**
      * TODO: add instance variables here.
-     *
      * List all instance variables of the Commit class here with a useful
      * comment above them describing what that variable represents and how that
      * variable is used. We've provided one example for `message`.
      */
 
     /** The message of this Commit. */
+    public static String HEAD;
     private String message;
+    private String date;
+    private String parent;
+    private String sh1;
+    void save()
+    {
+        File com = join(Repository.commit , sh1);
+        writeObject(com,this);
+        writeContents(Repository.HEAD,sh1);
+    }
+
+    public Commit(String message , String parent) {
+        // hard code the initial time
+        if (parent == null)
+        {
+            date = "00:00:00 UTC, Thursday, 1 January 1970";
+        }
+        else {
+
+        }
+        this.message = message;
+        this.parent = parent;
+        File f=join(Repository.CWD,"f");
+        writeObject(f,this);
+        byte[] byteArray =readContents(f);
+
+        this.sh1 = sha1(byteArray);
+        HEAD = sh1;
+        save();
+
+    }
 
     /* TODO: fill in the rest of this class. */
 }
--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -1,6 +1,14 @@
 package gitlet;
 
+
 import java.io.File;
+import java.io.IOException;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
+
 import static gitlet.Utils.*;
 
 // TODO: any imports you need here
@@ -15,10 +23,12 @@ public class Repository {
     /**
      * TODO: add instance variables here.
      *
+     *
      * List all instance variables of the Repository class here with a useful
      * comment above them describing what that variable represents and how that
      * variable is used. We've provided two examples for you.
      */
+
 
     /** The current working directory. */
     public static final File CWD = new File(System.getProperty("user.dir"));
@@ -26,4 +36,29 @@ public class Repository {
     public static final File GITLET_DIR = join(CWD, ".gitlet");
 
     /* TODO: fill in the rest of this class. */
+    public static File stage = join(GITLET_DIR,"staging area");
+    public static File commit = join(GITLET_DIR,"commits");
+    public static File addition = join(stage,"addition");
+    public static File removal = join(stage,"removal");
+    public static File HEAD = join(GITLET_DIR,"HEAD");
+
+
+
+
+    public static void init() throws IOException {
+        if(GITLET_DIR.exists())
+        {
+            System.out.println("A Gitlet version-control system already exists in the current directory.");
+            System.exit(0);
+        }
+        GITLET_DIR.mkdir();
+        stage.mkdir();
+        commit.mkdir();
+        addition.mkdir();
+        removal.mkdir();
+        HEAD.createNewFile();
+
+
+        new Commit("initial commit" , null);
+    }
 }
