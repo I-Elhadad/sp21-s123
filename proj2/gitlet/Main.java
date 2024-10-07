de15a77be91b3caae5c07c4cae6851039b933830
Ibrahem Elhdad <hemaelhadad26@gmail.com>
2024-10-08 00:50:27 +0300

    test

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -10,7 +10,7 @@ public class Main {
     /** Usage: java gitlet.Main ARGS, where ARGS contains
      *  <COMMAND> <OPERAND1> <OPERAND2> ...
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
 
         if(args.length==0)
         {
